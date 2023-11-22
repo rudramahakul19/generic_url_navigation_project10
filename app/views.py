447d7c39b0ea2d11bd032e60d6606d35093d81d3
rudramahakul19 <rudramahakul2014@gmail.com>
2023-11-22 13:18:57 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def rudra(request):
+    return render(request,'rudra.html')
+
+def lipika(request):
+    return render(request,'lipika.html')
